040724fa9e4bdfcabcc005b16599693c620ce8b3
tddebart <tddebart@gmail.com>
2022-05-23 16:25:00 +0200

    Initial Commit

---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿
+public class Game
+{
+    public const uint width = 1280;
+    public const uint height = 720;
+    private const string title = "Pong";
+    private RenderWindow window;
+    private readonly VideoMode mode = new(width, height);
+    private readonly Font font = new("./Resources/Fonts/Arial.ttf");
+
+    // fps counter stuff
+    private int framesRendered;
+    private int fps;
+    private DateTime lastTime;
+
+    public Game()
+    {
+        window = new RenderWindow(mode, title, Styles.Titlebar | Styles.Close);
+
+        window.SetVerticalSyncEnabled(true);
+        window.Closed += (_, _) => window.Close();
+    }
+
+    public void Run()
+    {
+        GuiImpl.Init(window);
+        while (window.IsOpen)
+        {
+            HandleEvents();
+            Update();
+            Draw();
+        }
+    }
+
+    private void HandleEvents()
+    {
+        window.DispatchEvents();
+    }
+
+    private void Update()
+    {
+        GuiImpl.Update(window, 0);
+    }
+    
+    private void Draw()
+    {
+        framesRendered++;
+        
+        window.Clear(Color.Black);
+
+        if ((DateTime.Now - lastTime).TotalSeconds >= 1)
+        {
+            // one second has elapsed 
+            fps = framesRendered;                     
+            framesRendered = 0;            
+            lastTime = DateTime.Now;
+        }
+        
+        window.PushGLStates();
+        window.Draw(new Text((fps).ToString(), font, 30));
+        window.PopGLStates();
+        
+        ImGui.ShowDemoWindow();
+
+        GuiImpl.Render(window);
+        
+        window.Display();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿global using ImGuiNET;
+global using ImGuiNETSFML;
+global using SFML.Graphics;
+global using SFML.Window;
+
+var game = new Game();
+game.Run();
